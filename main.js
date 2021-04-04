fa1a125979ed5ade0b3c3b5429d2e86a1dead83f
loong1998 <81916572+loong1998@users.noreply.github.com>
2021-04-05 05:34:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function bigPicture(pic) {
+    pic.style.height = "270px"
+    pic.style.width = "350px"
+}
+
+function normalPicture(pic) {
+    pic.style.height = "250px"
+    pic.style.width = "300px"
+}
+
